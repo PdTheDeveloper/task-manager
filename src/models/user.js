650f9927bd6622ba78d3bdfa7ae1ed0fc539de33
PdTheDeveloper <www.p.dadsetan@gmail.com>
2020-12-19 00:18:45 +0330

    User pre-save hash method

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User' , {
+const userSchema = new mongoose.Schema({
     name : {
         type : String ,
         required : true
@@ -11,6 +12,7 @@ const User = mongoose.model('User' , {
         required : true ,
         trim : true ,
         lowercase : true ,
+        // set : p => await bcrypt.hash(p , 8) ,
         validate(value) {
             if(!validator.isEmail(value)) {
                 throw new Error('The provided email is invalid!')
@@ -20,17 +22,29 @@ const User = mongoose.model('User' , {
     password : {
         type : String ,
         required : true ,
-        minlength : 7 ,
+        minlength : 7 , 
         validate(value) {
             if(value.toLowerCase().includes('password')) {
                 throw new Error("Your password must not contain the word 'password'!")
             }
-        }
+        } 
     } ,
     age : {
         type : Number ,
         default : 0
     }
 })
+
+userSchema.pre('save' , async function(next) {
+    const user = this
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password , 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User' , userSchema)
 
 module.exports = User
